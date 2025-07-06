dc262c6b77b65be9b094b7955856dee3d6c73191
bryan retrepo ortiz <bryanhit@gmail.com>
2025-07-06 10:45:56 -0500

    INITIAL COMMIT

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { defineConfig } from 'vite'
+import path from 'path'
+
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  plugins: [tanstackStart()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
+})
